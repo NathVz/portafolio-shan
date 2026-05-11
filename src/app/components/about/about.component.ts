e598c4284d58c3e2031bd996aa0899dda9af719c
Shan Sanchez <67060966+NathVz@users.noreply.github.com>
2026-05-11 16:34:02 -0600

    update cv

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -10,9 +10,9 @@ import { Component, signal } from '@angular/core';
 export class AboutComponent {
   name = signal('Shanath');
   role = signal('Front-end Developer | UX & UI');
-  description = signal(`
-    Desarrolladora web front-end especializada en Angular.
-    Con más de 5 años de experiencia, me he enfocado en construir interfaces de usuario intuitivas, accesibles y con un alto rendimiento.
-    Actualmente, ocupo el puesto de líder técnico, lo que me ha permitido adquirir experiencia no solo en el desarrollo, sino también en la toma de decisiones técnicas y la gestión de proyectos. He tenido la oportunidad de trabajar en diversos proyectos, tanto en entornos colaborativos como en solitario, lo que ha ampliado mis habilidades y conocimientos.
-  `);
+
+  description =
+    signal(`Soy desarrolladora web front-end con 6 años de experiencia, a lo largo de mi carrera he desarrollado múltiples aplicaciones web dando soluciones innovadoras, construyendo interfaces de
+usuario intuitivas, accesibles y de alto rendimiento.
+Actualmente, ocupo el puesto de líder técnico, lo que me ha permitido adquirir experiencia no solo en el desarrollo, sino también en la toma de decisiones técnicas y la gestión de proyectos.`);
 }
